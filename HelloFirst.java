e30f72b646543645f08b75e0e4386c7336ceb038
BESTLOSERUDIT <54463780+BESTLOSERUDIT@users.noreply.github.com>
2019-08-24 21:33:19 +0530

    Update HelloFirst.java

--- HelloFirst.java
+++ HelloFirst.java
@@ -3,17 +3,17 @@
  */
 public class HelloFirst {
   public static void main(String[] args) {
-    mail.host=smtp.gmail.com
-mail.username=//your gmail
-mail.password=//your password
-mail.defaultEncoding=UTF-8
-mail.smtp.auth=true
-mail.smtp.starttls.required=true
-mail.smtp.starttls.enable=true
-mail.smtp.socketFactory.class=javax.net.ssl.SSLSocketFactory
-mail.smtp.socketFactory.fallback=false
-mail.smtp.port=465
-mail.smtp.socketFactory.port=465
+    mail.host=smtp.gmail.com;
+mail.username=//your gmail;
+mail.password=//your password;
+mail.defaultEncoding=UTF-8;
+mail.smtp.auth=true;
+mail.smtp.starttls.required=true;
+mail.smtp.starttls.enable=true;
+mail.smtp.socketFactory.class=javax.net.ssl.SSLSocketFactory;
+mail.smtp.socketFactory.fallback=false;
+mail.smtp.port=465;
+mail.smtp.socketFactory.port=465;
     System.out.println("Hello, Udit bhai2 World");
   }
 }

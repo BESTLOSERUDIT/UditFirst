1b85f2d2778aaae4e204a855c785b7941252ebb8
BESTLOSERUDIT <54463780+BESTLOSERUDIT@users.noreply.github.com>
2019-08-24 17:10:40 +0530

    Update HelloFirst.java

--- HelloFirst.java
+++ HelloFirst.java
@@ -1,7 +1,7 @@
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
-public class HelloWorld {
+public class HelloFirst {
   public static void main(String[] args) {
     System.out.println("Hello, Udit bhai World");
   }
